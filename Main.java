be637baf5eb96f0f363f9e0efe1378eabb2bab06
EuGenii <90748705+EuGeniiNed@users.noreply.github.com>
2023-10-04 18:29:58 +0300

    Update Main.java
    Added a getter for the Cart and Order class, and also added code to main.

--- Main.java
+++ Main.java
@@ -3,7 +3,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        Product product = new Product(1, "Телефон", 499.99);
 
+        // Створення кошика та додавання продукту
+        Cart cart = new Cart();
+        cart.addProduct(product);
+
+        // Створення замовлення та виведення статусу
+        Order order = new Order(1, cart.getProducts());
+        System.out.println("Статус замовлення: " + order.getStatus());
     }
 }
 class Product {
@@ -31,6 +39,9 @@ class Cart {
         products.remove(product);
     }
 
+    public List<Product> getProducts() {
+        return products;
+    }
 }
 
 // Клас Order
@@ -45,12 +56,16 @@ class Order {
         this.status = "В обробці";
     }
 
+
     public String getStatus() {
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
+    }
+    public List<Product> getProducts() {
+        return products;
     }
 
 }
